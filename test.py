a11ad266d1a65feca2c854d1c445e669d469e575
unknown <Troy@Troy-PC.(none)>
2012-01-01 20:25:14 -0500

    a

--- test.py
+++ test.py
@@ -2,15 +2,15 @@ import pygame
 from pygame.locals import *
 
 
-class GameObject:
-	def __init__(self, image, height, speed):
-		self.speed = speed
-		self.image = image
-		self.pos = image.get_rect().move(0, height)
-	def move(self):
-		self.pos = self.pos.move(0, self.speed)
-		if self.pos.right > 600:
-			self.pos.left = 0
+# class GameObject:
+	# def __init__(self, image, height, speed):
+		# self.speed = speed
+		# self.image = image
+		# self.pos = image.get_rect().move(0, height)
+	# def move(self):
+		# self.pos = self.pos.move(0, self.speed)
+		# if self.pos.right > 600:
+			# self.pos.left = 0
 
 
 
@@ -21,21 +21,21 @@ background = pygame.image.load('art/floors.png').convert()
 screen.blit(background, (0,0))
 
 
-objects = []
-for x in range(10): 		#create 10 objects
-	o = GameObject(player, x*40, x)
-	objects.append(o)
-while 1:
-	for event in pygame.event.get():
-		if event.type in (QUIT, KEYDOWN):
-			sys.exit()
-	for o in objects:
-		screen.blit(background, o.pos, o.pos)
-		for o in objects:
-			o.move()
-			screen.draw(o.image, o.pos)
-		pygame.display.update()
-		pygame.time.delay(100)
+# objects = []
+# for x in range(10): 		#create 10 objects
+	# o = GameObject(player, x*40, x)
+	# objects.append(o)
+# while 1:
+	# for event in pygame.event.get():
+		# if event.type in (QUIT, KEYDOWN):
+			# sys.exit()
+	# for o in objects:
+		# screen.blit(background, o.pos, o.pos)
+		# for o in objects:
+			# o.move()
+			# screen.draw(o.image, o.pos)
+		# pygame.display.update()
+		# pygame.time.delay(100)
 
 
 pygame.time.delay(1000)
