9a75fe5a64012ccea1a356f4999105fabd79d4f5
unknown <Troy@Troy-PC.(none)>
2012-01-08 00:20:59 -0500

    cleaned up mapParser a bit and made it so tiles can come from multiple sources

--- mapGen.py
+++ mapGen.py
@@ -1,15 +1,22 @@
 x=30
 y=30
 
-tilefile = 'art/floors.png'
+floortilefile = 'art/floors.png'
 pos = "3:0"
-posborder = "2:3"
+
+bordertilefile = "art/dark brick.jpg"
+posborder = "0:0"
 
 file = open("maps/mapgen_map.map", "w")
-file.write("~Tile Source\n"+tilefile+"\n~Tile Size (x/y)\n32\n32\n~Map Size in Tiles (x/y)\n"+str(x)+"\n"+str(y)+"\n~Setup (Map col:row/Tile col:row)")
+file.write(">>> Tile Source [size x:y]\n" + \
+	"0-> "+floortilefile +" [32:32]\n" + \
+	"1-> "+bordertilefile+" [32:32]\n" + \
+	">>> Map Size in Tiles (x/y)\n" + \
+	str(x)+":"+str(y)+"\n" + \
+	">>> Setup (Source-> Map col:row/Tile col:row)")
 for i in range(x):
 	for j in range(y):
 		if (i==0) or (j==0) or (i == x-1) or (j == y-1):
-			file.write("\n"+str(i)+":"+str(j)+"/"+posborder+"(1)")
+			file.write("\n1-> "+str(i)+":"+str(j)+"/"+posborder+"(1)")
 		else:
-			file.write("\n"+str(i)+":"+str(j)+"/"+pos+"(0)")
+			file.write("\n0-> "+str(i)+":"+str(j)+"/"+pos+"(0)")
--- src/mapParser.py
+++ src/mapParser.py
@@ -8,108 +8,82 @@ def tileToMap(mapSurface, tileCoordsTo, tileSubsurface, squaresize):
 def genMap(file_base):
 	file = file_base + '.map'
 
-	tileFile, tileSize, mapSize, setup = parse(file)
-	img =  pg.Surface((mapSize[0]*tileSize[0], mapSize[1]*tileSize[1]))
-
-	tm = TileMap(tileFile, tileSize)
-
-	for pos in setup.keys():
-		tileToMap(img, pos, tm.get(setup[pos].type()), tileSize)
-	
-	if tileSize != g.TILE_RES:
-		print tileSize, g.TILE_RES
-		print "Warning: Tile size inconsistency"
-		raise KeyboardInterrupt
-
-	return {'img':img, 'tileFile':tileFile, 'tileSize':tileSize, 
-			'mapSize':mapSize, 'setup':setup}
-
-def parseMapLine(line):
-	pos, tile = line.split("/")
-
-	# construct map position x,y coords
-	pos = tuple([int(i) for i in pos.split(":")])
-
-	# tile source position (type)
-	tile = tile.split("(")
-	type = tuple([int(i) for i in tile[0].split(":")])
-	tile = tile[1]
-
-	# blocked = cannot be walked on (1 is blocked)
-	blocked = True if tile[:-1] == "1" else False
-
-	return pos, Tile(type, blocked)
-	
-
-def parse(file):
 	theMap = [line.strip() for line in open(file, 'r').readlines()]
 	
-	tileFile = theMap[1]
-	# Future: Check tileSize vs. TILE_RES
-	tileSize = tuple([int(theMap[3]), int(theMap[4])])
-	mapSize = tuple([int(theMap[6]), int(theMap[7])])
+	line = 1
+
+	tileFiles = []
+	while not theMap[line].startswith(">>>"):
+		tileFiles.append(theMap[line])
+		line+=1
+	for i in range(len(tileFiles)):
+		number, temp = tileFiles[i].split("-> ")
+		number = int(number)
+		source, xy = temp.split(" [")
+		xy = xy[:-1].split(":")
+		xy = (int(xy[0]), int(xy[1]))
+		tileFiles[i]=source
+		#check that all tileFiles have same res and that they match the given res
+		if xy != g.TILE_RES:
+			print "Warning: Tile size inconsistency from tile files"
+			raise KeyboardInterrupt
+
+	line+=1
+
+	mapSize = tuple([int(i) for i in theMap[line].split(":")])
+
+	line+=2
 	
-	# setup = [[],[]]
 	setup = {}
-	for line in theMap[9:]:
-		# first, second = each.split("/")
-		pos, tile = parseMapLine(line)
-		setup[pos] = tile
-	return tileFile, tileSize, mapSize, setup
+	for each in theMap[line:]:
+		source, temp = each.split("-> ")
+		source = int(source)
+
+		posOnMap, temp = temp.split("/")
+
+		# construct map position x,y coords
+		posOnMap = tuple([int(i) for i in posOnMap.split(":")])
+
+		# tile source position (type)
+		posOnTileFile, blocked = temp.split("(")
+		posOnTileFile = tuple([int(i) for i in posOnTileFile.split(":")])
+
+		# blocked = cannot be walked on (1 is blocked)
+		blocked = True if blocked[:-1] == "1" else False
+
+		setup[posOnMap] = Tile(source, posOnTileFile, blocked)
+
+	img =  pg.Surface((mapSize[0]*g.TILE_RES[0], mapSize[1]*g.TILE_RES[1]))
+
+	for i in range(len(tileFiles)):
+		tileFiles[i] = TileMap(tileFiles[i], g.TILE_RES)
+
+	for pos in setup.keys():
+		tileToMap(img, pos, tileFiles[setup[pos].source()].get(setup[pos].type()), g.TILE_RES)
+
+	return {'img':img, 'tileFile':tileFiles, 'tileSize':g.TILE_RES, 
+			'mapSize':mapSize, 'setup':setup}
+
+
 
 class Tile:
-	def __init__(self, type, blocked):
-		self.Type = type
+	def __init__(self, source, posOnTileFile, blocked):
+		self.Source = source
+		self.PosOnTileFile = posOnTileFile
 		self.Blocked = blocked
 		self.Blocked_orig = blocked
 		self.under = None
 		self.mapSubsurface = None
 	
+	def source(self):
+		return self.Source
+
 	def type(self):
-		return self.Type
+		return self.PosOnTileFile
 	
 	def blocked(self):
 		return self.Blocked
-	
-	'''
-	def addEvents(self, event, mapSubsurface):
-		self.Events.extend(event)
-		self.genEvent(mapSubsurface)
-		
-		
-		for each in self.Events:
-	
-	def genEvent(self, mapSubsurface):
-		evt = self.Events[0]
 
-		if evt.blocked:
-			self.Blocked = True
-		
-		# get the art
-		artfile, arttile = evt.imageInfo()
-		artfile = pg.image.load(artfile).convert_alpha()
-		
-		# place it on map
-		self.under = mapSubsurface.copy()
-		self.mapSubsurface = mapSubsurface
-		arttile = g.tile2rect(arttile, (32,32))
-		mapSubsurface.blit(artfile.subsurface(arttile), (0,0))
-		
-		# change tile info (such as blocked)
-	
-	def removeEvent(self):
-		# Event is finished, reinstate original blocked val
-		self.Blocked = self.Blocked_orig
-
-		del(self.Events[0])
-		
-		# replace old tile
-		self.mapSubsurface.blit(self.under, (0,0))
-		
-		# call genEvent on next event if there is one
-		if len(self.Events) >0:
-			self.genEvent(self.mapSubsurface)
-	'''
 	
 class TileMap:
 	def __init__(self, tileFile, squareSize):
